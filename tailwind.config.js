5cc2868d5b9211718a296ae8614eef39c2671384
ProfInvincible <muoghaluijeoma9@gmail.com>
2023-03-06 11:40:53 +0100

    message

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+// /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      ash: "#f5f5f5",
+      white: "#ffffff",
+      lightash: "#838282",
+      darkerash: "#383838",
+      midnightash: "#323131",
+      darkestash: "#1d1c1c",
+      purple: "#b845ff",
+
+      extend: {},
+    },
+    extend: {},
+  },
+  plugins: [],
+};
