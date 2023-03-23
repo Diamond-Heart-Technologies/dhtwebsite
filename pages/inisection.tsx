69e2b319773c3e1e859dc2ca6b6faf17300cd77b
Inioluwa <inioluwadeborah2@gmail.com>
2023-03-23 18:08:48 +0100

    backgroundadjustments

--- pages/inisection.tsx
+++ pages/inisection.tsx
@@ -48,13 +48,13 @@ export const OurProcess = () => {
       <div
         className={`mx-auto mt-24 text-white py-8 hidden sm:block ${ourprocessstyles.DesktopBg}`}
       >
-        <h2 className="text-center text-2xl sm:mt-24 lg:mt-80">Our Process</h2>
+        <h2 className="text-center text-2xl sm:mt-96 lg:mt-80">Our Process</h2>
         <OurProcessCardMapping />
       </div>
 
       {/* mobile */}
       <div
-        className={`mx-auto mt-24 text-white py-20 mob:block sm:hidden ${ourprocessstyles.MobileBg}`}
+        className={`mx-auto mt-24 text-white py-20 block sm:hidden ${ourprocessstyles.MobileBg}`}
       >
         <h2 className="text-center text-2xl mt-60 sm:mt-16 lg:mt-28">
           Our Process
